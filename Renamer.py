d6d4af5baed2c81e3860d1aa685c99537c30086c
kohawk <52754811+kohawk@users.noreply.github.com>
2022-04-07 10:24:54 +0300

    Add files via upload
    Readability update

--- Renamer.py
+++ Renamer.py
@@ -10,14 +10,24 @@ directory = os.getcwd()
 
 count = 0
 
+def GetArtistName(number):
+    url = f"https://music.yandex.ru/track/{number}"
+    RawText = requests.get(url)
+    LxmlText = BeautifulSoup(RawText.text, "lxml")
+    ArtistName = LxmlText.find("div", "sidebar__info sidebar__info-short").find("a", "d-link deco-link")
+    return ArtistName.text
+
+def GetSongTitle(number):
+    url = f"https://music.yandex.ru/track/{number}"
+    RawText = requests.get(url)
+    LxmlText = BeautifulSoup(RawText.text, "lxml")
+    SongTitle = LxmlText.find("div", "sidebar__title sidebar-track__title deco-type typo-h2").find("a", "d-link deco-link")
+    return SongTitle.text
+
 for filename in os.listdir(directory):
     if filename.endswith(".mp3"):
         filename = os.path.splitext(filename)[0]
-        url = f"https://music.yandex.ru/track/{filename}"
-        RawText = requests.get(url)
-        SongTitle = BeautifulSoup(RawText.text, "lxml").find("div", "sidebar__title sidebar-track__title deco-type typo-h2").find("a", "d-link deco-link").text
-        ArtistName = BeautifulSoup(RawText.text, "lxml").find("div", "sidebar__info sidebar__info-short").find("a", "d-link deco-link").text
-        os.rename(os.path.join(directory, f"{filename}.mp3"), f"renamed\{ArtistName} - {SongTitle}".strip() + ".mp3")
+        os.rename(os.path.join(directory, f"{filename}.mp3"), f"renamed\{GetArtistName(filename)} - {GetSongTitle(filename)}".strip() + ".mp3")
         count += 1
 
 print(f"Renamed {count} songs")
